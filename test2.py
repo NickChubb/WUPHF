b87a42ca4179cde3bf30ddcfe24b548fcc01cc3f
Jordan <jla522@sfu.ca>
2020-01-12 00:21:17 -0800

    making library

--- test2.py
+++ test2.py
@@ -1,27 +1,25 @@
 from fbchat import Client
 from fbchat.models import *
-from getpass import getpass
-
-
-#email = input("Email: ")
-#password = getpass()
+import wuphf as w
 
 email = "ryanstartedthefire69@gmail.com"
 password = "1Agas3rs"
+name = "jordan"
+msg = "HIHIHIHIHIHIHI"
 
-client = Client(email, password)
+#w.sendMsg(email, password, name, msg)
 
-if not client.isLoggedIn():
-	print("fuck")
+class EchoBot(Client):
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+        self.markAsDelivered(thread_id, message_object.uid)
+        self.markAsRead(thread_id)
 
-name = input("Input the users name: ")
-users = client.searchForUsers(name)
-user = users[0]
+        log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
 
-#print(user)
-
-client.send(Message("Test from messenger API"), user.uid, thread_type=ThreadType.USER)
+        # If you're not the author, echo
+        if author_id != self.uid:
+            self.send(message_object, thread_id=thread_id, thread_type=thread_type)
 
 
-
-client.logout()
+client = EchoBot(email, password)
+client.listen()
